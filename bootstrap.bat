d6ae0437e85a47eb43f6255467a621e04c26c6f8
Matt Jaeger <matthew.jaeger@gmail.com>
2018-01-07 21:52:33 -0800

    x86 vs x64 installation. update jaegernetserver

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,11 +1,11 @@
 mkdir build & cd build
 mkdir x86-windows & cd x86-windows
 
-cmake -G "Visual Studio 15 2017" "-DCMAKE_TOOLCHAIN_FILE=../../Dependencies/scripts/buildsystems/vcpkg.cmake" "-DCMAKE_INSTALL_PREFIX=../../installed" "-DCMAKE_PREFIX_PATH=../../Dependencies/installed/x86-windows/share" ../../Src
+cmake -G "Visual Studio 15 2017" "-DCMAKE_TOOLCHAIN_FILE=../../Dependencies/scripts/buildsystems/vcpkg.cmake" "-DCMAKE_INSTALL_PREFIX=../../installed/x86-windows" "-DCMAKE_PREFIX_PATH=../../Dependencies/installed/x86-windows/share" ../../Src
 
 cd ..
 mkdir x64-windows & cd x64-windows
 
-cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_TOOLCHAIN_FILE=../../Dependencies/scripts/buildsystems/vcpkg.cmake" "-DCMAKE_INSTALL_PREFIX=../../installed" "-DCMAKE_PREFIX_PATH=../../Dependencies/installed/x64-windows/share" ../../Src
+cmake -G "Visual Studio 15 2017 Win64" "-DCMAKE_TOOLCHAIN_FILE=../../Dependencies/scripts/buildsystems/vcpkg.cmake" "-DCMAKE_INSTALL_PREFIX=../../installed/x64-windows" "-DCMAKE_PREFIX_PATH=../../Dependencies/installed/x64-windows/share" ../../Src
 
 cd ../..
